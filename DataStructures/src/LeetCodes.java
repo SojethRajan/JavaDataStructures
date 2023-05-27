07daeb57f750445b0d6a366dc2b32058065345ca
SojethRajan <sojetha@gmail.com>
2023-05-27 16:04:51 +0530

    modified leetCode class

--- DataStructures/src/LeetCodes.java
+++ DataStructures/src/LeetCodes.java
@@ -1,9 +1,11 @@
 public class LeetCodes{
     class Solution {
-        public int romanToInt(String s) {
+        public int romanToInt(String s){
             String one = "I";
             String five = "V";
             String ten = "X";
+
+            return 8;
         }
     }
 }
